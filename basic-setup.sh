6e802d65c3f5222dea52f7c25969b3195307873b
Benjamin Blondeau <benjamin.blondeau@t1-bb-research.lxd>
2020-03-06 14:03:23 +0000

    changed sudo access basic-setup

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-sudo -s
 echo "sudo privilege acquired"
 echo "automatic set-up of dependencies ..."
-apt-get install htop
-apt-get install gzip
-apt-get install tmux
-apt-get install zsh
-apt intall wget git
-chsh -s /usr/bin/zsh root
+sudo apt-get install htop
+sudo apt-get install gzip
+sudo apt-get install tmux
+sudo apt-get install zsh
+sudo apt-get install wget git
+sudo chsh -s /usr/bin/zsh root
 echo $SHELL
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "installations completed"
